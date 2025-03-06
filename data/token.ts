cb9f61dd51cb4f89aac01d786a0ed032e2e77012
FjrREPO <jossfajar21@gmail.com>
2025-03-07 01:40:06 +0700

    Update token addresses for Sonic Blaze Testnet

--- data/token.ts
+++ data/token.ts
@@ -2,7 +2,7 @@ export const TOKEN_DATA = [
   {
     "name": "SONIC",
     "symbol": "SONIC",
-    "address": "0x8238654830969A21adeB1D4c3d8D9b72d1dFbd7B",
+    "address": "0xB8AEAD172f82B4bCC5F001Df218C3f6F84527359",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/32684.png",
@@ -12,7 +12,7 @@ export const TOKEN_DATA = [
   {
     "name": "Ethereum",
     "symbol": "ETH",
-    "address": "0x14cA5409727F55b9b9E2CA356c8489a40441B16c",
+    "address": "0x09C0Ce218899706430C0eB0E0672D66Fc880b3d6",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
@@ -22,7 +22,7 @@ export const TOKEN_DATA = [
   {
     "name": "Bitcoin",
     "symbol": "BTC",
-    "address": "0x8e3D5B26Cfe650123a822dE0E1f09fa680a30278",
+    "address": "0x5af60e853CF5f061AD2dBee25dFE64F277DBEC01",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
@@ -32,7 +32,7 @@ export const TOKEN_DATA = [
   {
     "name": "Wrapped Ether",
     "symbol": "WETH",
-    "address": "0x1ad4a7A80f5548923C8E5627AbAfB977dfe014FF",
+    "address": "0xe0a46B4CB0069fE8BFd8a255B8C77ACEDFDD1bcd",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png",
@@ -42,7 +42,7 @@ export const TOKEN_DATA = [
   {
     "name": "PEPE",
     "symbol": "PEPE",
-    "address": "0x9D2DE5616C423dc52CFC3332111802a48E8E5C6f",
+    "address": "0xFbE583EC5E062728C6Ae28b9eD1CF16bF3D44052",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://dd.dexscreener.com/ds-data/tokens/solana/B5WTLaRwaUQpKk7ir1wniNB6m5o8GgMrimhKMYan2R6B.png",
@@ -52,7 +52,7 @@ export const TOKEN_DATA = [
   {
     "name": "OFFICIAL TRUMP",
     "symbol": "TRUMP",
-    "address": "0xF6ac8403a2050fF2AEA572B01f7BA085f054f7B5",
+    "address": "0x95Bd809c55711fA7c7e6Ba4C860A274e475FE2a3",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/35336.png",
@@ -62,7 +62,7 @@ export const TOKEN_DATA = [
   {
     "name": "DOGE AI",
     "symbol": "DOGEAI",
-    "address": "0x191EB4C970B1A972C4fc4517A9f110Ebd40deE05",
+    "address": "0x66D289fF71AcA3b2Dd38fE3aF29dD8D8aA734829",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/35601.png",
@@ -72,7 +72,7 @@ export const TOKEN_DATA = [
   {
     "name": "dogwifhat",
     "symbol": "WIF",
-    "address": "0x458Df501e4aee7f6380894E321e998f19A976bFa",
+    "address": "0xe8451691d43AE8C3991Ae087731249E5C29d3e41",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/28752.png",
@@ -82,7 +82,7 @@ export const TOKEN_DATA = [
   {
     "name": "STONKS",
     "symbol": "STONKS",
-    "address": "0xF1AF6fd2726213278A221F9D6FF09B0EDf9d8888",
+    "address": "0x89E5697915A12f8448B27F00795754E3dFE8D69b",
     "chain": "Sonic Blaze Testnet",
     "decimals": 18,
     "logoURI": "https://s2.coinmarketcap.com/static/img/coins/200x200/29560.png",
